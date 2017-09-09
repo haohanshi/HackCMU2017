86d42bb4660e09fac7d89461709a55a2764c1793
Haohan Shi <haohans@andrew.cmu.edu>
2017-09-09 13:50:13 -0400

    bug fix

--- main.py
+++ main.py
@@ -17,8 +17,20 @@ def main(startPoint, endPoint):
     return result
 
 def findRoute(Paths, startPoint, endPoint):
-    route = []
-    route = hardcode.route1
+    route1 = []
+    # for key in Paths:
+    #     path = Paths.key
+    #     if path.cp1.name == startPoint[0] and path.legal:
+
+    #HARDCODE FOR TEST ONLY
+    route1.append(Paths["WEH5312_to_WEH5300 wing"])
+    route1.append(Paths["WEH5300 wing_to_entrance to DH"])
+    route1.append(Paths["entrance to DH_to_level A entrance to DH"])
+    route1.append(Paths["level A entrance to DH_to_west staircase on DH level A"])
+    route1.append(Paths["west staircase on DH level A_to_west staircase on DH level 1"])
+    route1.append(Paths["west staircase on DH level 1_to_west staircase on DH level 2"])
+    route1.append(Paths["west staircase on DH level 2_to_DH2300 wing"])
+    route1.append(Paths["DH2300 wing_to_DH2210"])
     # for key in Paths:
     #     path = Paths[key]
     #     if path.cp1.name == startPoint[0] and path.legal:
@@ -34,5 +46,5 @@ def findRoute(Paths, startPoint, endPoint):
     # route.append(Paths["DH_1F_WestStair_to_DH_2F_WestStair"])
     # route.append(Paths["DH_2F_WestStair_to_DH_2350_Corridor"])
     # route.append(Paths["DH_2350_Corridor_to_DH2210"])
-    return route
+    return route1
 
