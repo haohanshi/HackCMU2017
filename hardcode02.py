c45c2647a6973f1d8290c1d90b93c1f96c08e470
Catherine <lili@Cats-Mac.wv.cc.cmu.edu>
2017-09-09 16:52:03 -0400

    hardcode02 2.0

--- hardcode02.py
+++ hardcode02.py
@@ -5,21 +5,15 @@ route2 = []
 
 #HARDCODE FOR TEST ONLY
 route2 append(Paths["Randy Pausch Bridge_to_Gates 5th floor entrance"])
-route2 append(Paths["Gates 5th floor entrance_to_Gates 5th floor common area"])
-route2 append(Paths["Gates 5th fllor common area_to_Gates 5th floor staircase"])
+route2 append(Paths["Gates 5th floor entrance_to_Gates 5th floor staircase"])
 route2 append(Paths["Gates 5th floor staircase_to_Gates 4th floor staircase"])
-route2 append(Paths["Gates 4th floor staircase_to_Gates 4th floor corridor"])
-route2 append(Paths["Gates 4th floor corridor_to_Gates to NSH entrance"])
-route2 append(Paths["Gates to NSH entrance_to_Gates to NSH bridge"])
-route2 append(Paths["Gates to NSH bridge_to_NSH 4th floor corridor"])
+route2 append(Paths["Gates 4th floor staircase_to_Gates to NSH bridge"])
+route2 append(Paths["Gates to NSH bridge_to_NSH to WEH bridge"])
 route2 append(Paths["NSH 4th floor corridor_to_NSH to WEH bridge"])
-route2 append(Paths["NSH to WEH bridge_to_WEH library"])
-route2 append(Paths["WEH library_to_WEH 4100 corridor"])
-route2 append(Paths["WEH 4100 corridor_to_WEH to Scott Entrance"])
-route2 append(Paths["WEH to Scott Entrance_to_WEH to Scott Bridge"])
+route2 append(Paths["WEH 4100 bridge_to_WEH to Scott Bridge"])
 route2 append(Paths["WEH to Scott Bridge_to_Scott Hall Cafe"])
 route2 append(Paths["Scott Hall Cafe_to_Scott to HH Corridor"])
 route2 append(Paths["Scott to HH Corridor_to_HH Elevator"])
 route2 append(Paths["HH Elevator_to_HH 1300 Wing"])
-route2 append(Paths["HH 1300 Wing_to_HH 1305"])
+
 
