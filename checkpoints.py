6f094b81c88714423e255ccd8dd079ad927c43dd
Catherine <lili@Cats-Mac.wv.cc.cmu.edu>
2017-09-09 11:50:55 -0400

    upload cpImages and edit checkpoints.py

--- checkpoints.py
+++ checkpoints.py
@@ -1,22 +1,29 @@
+#Uncommented DH_AF_MidElevator line13 
+#added DH_1F_Entrance_SmallDoor line15
+#Uncommented DH_1F_EastStair line17 
+#Uncommented DH_2F_EastStair line24
+#Uncommented DH_AF_MidElevator line13
+#Uncommented DH_2F_MidStair line23
+#Uncommented DH_2F_MidElevator line26
 CPList=[\
     ["WEH_DH_Entrance_DHside","Entrance","DH",0,0000],
     ["DH_AF_WestStair","Stair","DH",0,0355],
     ["DH_AF_MidStair","Stair","DH",0,0236],
     ["DH_AF_WestElevator","Elevator","DH",0,0353],
-    # ["DH_AF_MidElevator","Elevator","DH",0,0235],
+    ["DH_AF_MidElevator","Elevator","DH",0,0235],
     ["DH_AF_Corridor","Corridor","DH",0,0351],
-    
+    ["DH_1F_Entrance_SmallDoor","Entrance","DH",1,1000]
     ["DH_1F_WestStair","Stair","DH",1,1235],
-    # ["DH_1F_EastStair","Stair","DH",1,1142],
+    ["DH_1F_EastStair","Stair","DH",1,1142],
     ["DH_1F_WestElevator","Elevator","DH",1,1234],
     ["DH_1250_Corridor","Corridor","DH",1,1250],
     ["DH_1150_Corridor","Corridor","DH",1,1150],
     
     ["DH_2F_WestStair","Stair","DH",2,2351],
-    # ["DH_2F_MidStair","Stair","DH",2,2224],
-    # ["DH_2F_EastStair","Stair","DH",2,2135],
+    ["DH_2F_MidStair","Stair","DH",2,2224],
+    ["DH_2F_EastStair","Stair","DH",2,2135],
     ["DH_2F_WestElevator","Elevator","DH",2,2352],
-    # ["DH_2F_MidElevator","Elevator","DH",2,2252],
+    ["DH_2F_MidElevator","Elevator","DH",2,2252],
     ["DH2315","Room","DH",2,2315],
     ["DH2210","Room","DH",2,2210],
     ["DH_2350_Corridor","Corridor","DH",2,2350],
